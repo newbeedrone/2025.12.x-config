5860fe68dad0cb4b554076d0a179daa8b734cee0
Liuyongnan123 <lyn@sequoiatech.co>
2025-12-29 10:33:40 +0800

    remove other targets

--- configs/ARK_FPV/config.c
+++
@@ -1,7 +0,0 @@
-#include "drivers/io.h"
-
-void configTargetPreInit(void)
-{
-    ioPreinitByIO(IOGetByTag(IO_TAG(VDD_3V3_SENSORS1_EN)), IOCFG_OUT_PP, PREINIT_PIN_STATE_HIGH);
-    ioPreinitByIO(IOGetByTag(IO_TAG(VDD_3V3_SD_CARD_EN)), IOCFG_OUT_PP, PREINIT_PIN_STATE_HIGH);
-}
